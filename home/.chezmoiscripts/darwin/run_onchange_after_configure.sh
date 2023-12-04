b0fd715e093d5c936f51246c32d564d2d387fce4
Nemanja Zbiljić <nemanja.zbiljic@gmail.com>
2023-12-04 11:15:15 +0100

    remove Spotlight from defaults setup

--- home/.chezmoiscripts/darwin/run_onchange_after_configure.sh
+++ home/.chezmoiscripts/darwin/run_onchange_after_configure.sh
@@ -298,21 +298,6 @@ defaults write com.apple.mail DisableInlineAttachmentViewing -bool true
 defaults write com.apple.mail SpellCheckingBehavior -string "NoSpellCheckingEnabled"
 
 ###############################################################################
-# Spotlight                                                                   #
-###############################################################################
-
-# Disable Spotlight indexing for any volume that gets mounted and has not yet
-# been indexed before.
-# Use `sudo mdutil -i off "/Volumes/foo"` to stop indexing any volume.
-sudo defaults write /.Spotlight-V100/VolumeConfiguration Exclusions -array "/Volumes"
-# Load new settings before rebuilding the index
-killall mds > /dev/null 2>&1
-# Make sure indexing is enabled for the main volume
-sudo mdutil -i on / > /dev/null
-# Rebuild the index from scratch
-sudo mdutil -E / > /dev/null
-
-###############################################################################
 # Terminal & iTerm 2                                                          #
 ###############################################################################
 
