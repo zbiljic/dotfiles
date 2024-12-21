5279e9b5d9b5b61ba5d02bd27db6668a8972c686
Nemanja Zbiljić <nemanja.zbiljic@gmail.com>
2024-12-21 14:06:22 +0100

    update wezterm configuration

--- home/private_dot_config/wezterm/wezterm.lua
+++ home/private_dot_config/wezterm/wezterm.lua
@@ -10,6 +10,21 @@ return {
   -- If right padding is set to 0 then it will be increased
   -- to a single cell width
   enable_scroll_bar = true,
+
   -- How many lines of scrollback you want to retain per tab
   scrollback_lines = 65536,
+
+  -- Controls the maximum frame rate used when rendering easing effects
+	-- https://wezfurlong.org/wezterm/config/lua/config/animation_fps.html
+	animation_fps = 30,
+
+	-- Limits the maximum number of frames per second that wezterm will attempt to draw.
+  -- Defaults to 60.
+	-- https://wezfurlong.org/wezterm/config/lua/config/max_fps.html
+  max_fps = 120,
+
+  -- Whether to display a confirmation prompt when the window is closed by
+  -- the windowing environment, either because the user closed it with
+  -- the window decorations, or instructed their window manager to close it.
+	window_close_confirmation = "NeverPrompt",
 }
